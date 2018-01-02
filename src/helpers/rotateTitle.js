072bb1ddd502790737e031d3abaf91198919ced0
Tyler <tcameronsmith@gmail.com>
2018-01-02 19:51:02 +1100

    edit funciton to add timeout instead of ongoing interval

--- src/helpers/rotateTitle.js
+++ src/helpers/rotateTitle.js
@@ -3,23 +3,46 @@ function rotateTitle() {
     const titlesArray = [];
     titles.forEach(item => titlesArray.push(item));
 
-    setInterval(function () {
-        let activeIndex = titlesArray.findIndex(x => (x.className.includes('visible')));
+    // setInterval(function () {
+    //     let activeIndex = titlesArray.findIndex(x => (x.className.includes('visible')));
 
-        if (activeIndex === titlesArray.length - 1) {
-            titlesArray[activeIndex].classList.remove('visible');
-            titlesArray[activeIndex].classList.add('hidden');
+    //     if (activeIndex === titlesArray.length - 1) {
+    //         titlesArray[activeIndex].classList.remove('visible');
+    //         titlesArray[activeIndex].classList.add('hidden');
 
-            titlesArray[0].classList.remove('hidden');
-            titlesArray[0].classList.add('visible');
-        } else {
-            titlesArray[activeIndex].classList.remove('visible');
-            titlesArray[activeIndex].classList.add('hidden');
+    //         titlesArray[0].classList.remove('hidden');
+    //         titlesArray[0].classList.add('visible');
+    //     } else {
+    //         titlesArray[activeIndex].classList.remove('visible');
+    //         titlesArray[activeIndex].classList.add('hidden');
 
-            titlesArray[activeIndex + 1].classList.remove('hidden');
-            titlesArray[activeIndex + 1].classList.add('visible');
-        }
-    }, 1200);
+    //         titlesArray[activeIndex + 1].classList.remove('hidden');
+    //         titlesArray[activeIndex + 1].classList.add('visible');
+    //     }
+    // }, 1200);
+    
+    let timer = 1200;
+
+    for (let i = 1; i < titlesArray.length; i++) {
+        setTimeout(() => {
+            let activeIndex = titlesArray.findIndex(x => (x.className.includes('visible')));
+
+            if (activeIndex === titlesArray.length - 1) {
+                titlesArray[activeIndex].classList.remove('visible');
+                titlesArray[activeIndex].classList.add('hidden');
+
+                titlesArray[0].classList.remove('hidden');
+                titlesArray[0].classList.add('visible');
+            } else {
+                titlesArray[activeIndex].classList.remove('visible');
+                titlesArray[activeIndex].classList.add('hidden');
+
+                titlesArray[activeIndex + 1].classList.remove('hidden');
+                titlesArray[activeIndex + 1].classList.add('visible');
+            }            
+        }, timer);
+        timer += 1100;
+    }
 };
 
 export default rotateTitle;
