974dda30131c43e1bb47e4587b1ed2a084817f8c
Tyler <tcameronsmith@gmail.com>
2018-01-03 21:35:13 +1100

    create spotlight function for banner to reveal bg on mouseover

---
+++ src/helpers/spotlight.js
@@ -0,0 +1,13 @@
+function spotlight() {
+    // window.addEventListener("load", function (event) {
+    const spotlight = document.getElementById("banner-filter");
+
+    spotlight.style.background = "#fff";
+
+    //register an event to move the spotlight
+    document.onmousemove = function (event) {
+        spotlight.style.background = `-webkit-radial-gradient(${event.pageX}px calc(${event.pageY}px - 80px), 180px 180px, transparent 15%, #fff)`;
+    };
+};
+
+export default spotlight;
