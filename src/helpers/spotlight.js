b887a27f15ed7f681ef949179d534b3148dddb69
Tyler <tcameronsmith@gmail.com>
2018-01-06 09:55:17 +1100

    remove transparnt and replace with white color transparent

--- src/helpers/spotlight.js
+++ src/helpers/spotlight.js
@@ -6,7 +6,7 @@ function spotlight() {
 
     //register an event to move the spotlight
     document.onmousemove = function (event) {
-        spotlight.style.background = `-webkit-radial-gradient(${event.pageX}px calc(${event.pageY}px - 80px), 180px 180px, transparent 15%, #fff)`;
+        spotlight.style.background = `-webkit-radial-gradient(${event.pageX}px calc(${event.pageY}px - 80px), 180px 180px, rgba(255,255,255,0) 15%, #fff)`;
     };
 };
 
