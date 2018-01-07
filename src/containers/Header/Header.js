941531146fb6246d36923ee08af66128720a5a10
Tyler <tcameronsmith@gmail.com>
2018-01-08 09:02:05 +1100

    add style sheet for header

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import './style.css';
+import './style.css';
 import Logo from '../../components/Logo/Logo';
 import Nav from '../../components/Nav/Nav';
 
 const Header = () => {
     return (
-        <header className="App-header">
+        <header className="header">
             <Logo title="Testing Name" />
             <Nav />
         </header>
