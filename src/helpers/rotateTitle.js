f365461e0c84b8147c7b8f336982a4466f57b28a
Tyler <tcameronsmith@gmail.com>
2018-01-07 22:28:50 +1100

    remove unused code

--- src/helpers/rotateTitle.js
+++ src/helpers/rotateTitle.js
@@ -21,17 +21,6 @@ function rotateTitle() {
     //         titlesArray[activeIndex + 1].classList.add('visible');
     //     }
     // }, 1200);
-
-    const container = document.querySelector('.inner');
-    setTimeout(() => {
-        container.classList.add('attention');
-    }, 7000);
-    setTimeout(() => {
-        container.classList.remove('attention');
-    }, 8000);
-    
-
-
     
     let timer = 8500;
 
@@ -40,52 +29,25 @@ function rotateTitle() {
             let activeIndex = titlesArray.findIndex(x => (x.className.includes('visible')));
 
             if (activeIndex === titlesArray.length - 1) {
-                titlesArray[activeIndex].classList.remove('visible');
-                titlesArray[activeIndex].classList.add('hidden');
-
-                titlesArray[0].classList.remove('hidden');
+                titlesArray[activeIndex].classList.remove('visible');                
+                
                 titlesArray[0].classList.add('visible');
-
-                // titlesArray[0].classList.add('attention');
-                // titlesArray[0].classList.remove('attention');
             } else {
-                titlesArray[activeIndex].classList.remove('visible');
-                titlesArray[activeIndex].classList.add('hidden');
-
-                titlesArray[activeIndex + 1].classList.remove('hidden');
+                titlesArray[activeIndex].classList.remove('visible');                
+                
                 titlesArray[activeIndex + 1].classList.add('visible');
-
-                // titlesArray[activeIndex + 1].classList.add('attention');
             }            
         }, timer);
 
         setTimeout(() => {
-            let activeIndex = titlesArray.findIndex(x => (x.className.includes('visible')));            
-
-            // titlesArray[activeIndex].classList.add('attention');
-
             banner.classList.add('border');
-            // if (activeIndex === titlesArray.length - 1) {
-
-            //     titlesArray[0].classList.remove('bg');
-            //     // titlesArray[0].classList.remove('bg');
-            // } else {
-            //     titlesArray[activeIndex + 1].classList.remove('bg');
-            // }
         }, timer - 300);
 
         setTimeout(() => {
-
             banner.classList.remove('border');
-            // if (activeIndex === titlesArray.length - 1) {
-
-            //     titlesArray[0].classList.remove('bg');
-            //     // titlesArray[0].classList.remove('bg');
-            // } else {
-            //     titlesArray[activeIndex + 1].classList.remove('bg');
-            // }
         }, timer + 700);
-        timer += 1500;
+
+        timer += 1800;
     }
 };
 
