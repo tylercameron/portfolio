c3d7f10a2a1cb01009746832f57b9aa49548d161
Tyler <tcameronsmith@gmail.com>
2018-01-07 15:08:38 +1100

    add timeout for banner class to have top border on rotate event

--- src/helpers/rotateTitle.js
+++ src/helpers/rotateTitle.js
@@ -1,5 +1,6 @@
 function rotateTitle() {
     const titles = document.querySelectorAll('.banner-title');
+    const banner = document.querySelector('.banner');
     const titlesArray = [];
     titles.forEach(item => titlesArray.push(item));
 
@@ -20,8 +21,19 @@ function rotateTitle() {
     //         titlesArray[activeIndex + 1].classList.add('visible');
     //     }
     // }, 1200);
+
+    const container = document.querySelector('.inner');
+    setTimeout(() => {
+        container.classList.add('attention');
+    }, 7000);
+    setTimeout(() => {
+        container.classList.remove('attention');
+    }, 8000);
     
-    let timer = 7000;
+
+
+    
+    let timer = 8500;
 
     for (let i = 1; i < titlesArray.length; i++) {
         setTimeout(() => {
@@ -33,15 +45,47 @@ function rotateTitle() {
 
                 titlesArray[0].classList.remove('hidden');
                 titlesArray[0].classList.add('visible');
+
+                // titlesArray[0].classList.add('attention');
+                // titlesArray[0].classList.remove('attention');
             } else {
                 titlesArray[activeIndex].classList.remove('visible');
                 titlesArray[activeIndex].classList.add('hidden');
 
                 titlesArray[activeIndex + 1].classList.remove('hidden');
                 titlesArray[activeIndex + 1].classList.add('visible');
+
+                // titlesArray[activeIndex + 1].classList.add('attention');
             }            
         }, timer);
-        timer += 1100;
+
+        setTimeout(() => {
+            let activeIndex = titlesArray.findIndex(x => (x.className.includes('visible')));            
+
+            // titlesArray[activeIndex].classList.add('attention');
+
+            banner.classList.add('border');
+            // if (activeIndex === titlesArray.length - 1) {
+
+            //     titlesArray[0].classList.remove('bg');
+            //     // titlesArray[0].classList.remove('bg');
+            // } else {
+            //     titlesArray[activeIndex + 1].classList.remove('bg');
+            // }
+        }, timer - 300);
+
+        setTimeout(() => {
+
+            banner.classList.remove('border');
+            // if (activeIndex === titlesArray.length - 1) {
+
+            //     titlesArray[0].classList.remove('bg');
+            //     // titlesArray[0].classList.remove('bg');
+            // } else {
+            //     titlesArray[activeIndex + 1].classList.remove('bg');
+            // }
+        }, timer + 700);
+        timer += 1500;
     }
 };
 
