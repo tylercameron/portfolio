a89c60d4f2bae82d506791cade686e2f542bcfb5
Tyler <tcameronsmith@gmail.com>
2018-01-11 21:15:32 +1100

    add scroll animation to about componnent

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 // import { Link, Route } from 'react-router-dom';
 import aboutData from '../data/aboutData';
+// import ScrollAnimation from 'react-animate-on-scroll';
+import * as Scroll from 'react-scroll';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 import './style.css';
 
@@ -15,6 +18,45 @@ class About extends Component {
 
         this.descriptionInput = this.descriptionInput.bind(this);
     }
+    componentDidMount() {
+
+        Events.scrollEvent.register('begin', function (to, element) {
+            console.log("begin", arguments);
+        });
+
+        Events.scrollEvent.register('end', function (to, element) {
+            console.log("end", arguments);
+        });
+
+        scrollSpy.update();
+
+    }
+    componentWillUnmount() {
+        Events.scrollEvent.remove('begin');
+        Events.scrollEvent.remove('end');
+    }
+
+    handleSetActive(to) {
+        console.log('to', to);
+        const activeView = document.getElementById(to);
+
+        if (activeView.classList.contains('opac')) {
+            activeView.classList.remove('opac');
+        }
+
+        const children = activeView.children;
+
+        for (let i = 0; i < children.length; i++) {
+            // const element = array[i];
+            // console.log(children[i].children[1].children);
+            const ratingsDiv = children[i].children[1].children[0];
+            // console.log(ratingsDiv.classList);
+            if (ratingsDiv.classList.contains('hidden')) {
+                ratingsDiv.classList.remove('hidden');
+            }
+        }
+
+    }
 
     descriptionInput(e) {
         this.setState({ 
@@ -28,8 +70,12 @@ class About extends Component {
 
         return (
             <div id="about">
-                <h2>## TITLE</h2>
-                <label htmlFor="description-length">I don't want to know much &nbsp;
+                <Link activeClass="active" to="test1" spy={true} smooth={true} offset={-50} duration={500} onSetActive={this.handleSetActive}>
+                    My Tech Stack
+                </Link>
+                <h2 className="about-header">About Me</h2>
+                <h4 className="slider-title">How much do you want to know?</h4>
+                <label htmlFor="description-length">Not much &nbsp;
                     <input type="range" id="description-length" min="0" value={descriptionSlider} max="10" onChange={this.descriptionInput} />
                     &nbsp; I want all the details!
                 </label>
@@ -39,10 +85,43 @@ class About extends Component {
                         {description}
                     </p>
                 </div>
-                <div className="tech-stack">
-                    <h3 className="stack-title">Javascript</h3>
-                    <div className="stack-box"><div className="js-rating"></div></div>
-                </div>
+                <Element id="test1" className="tech-stack opac">
+                    <div className="tech-stack__item">
+                        <h3 className="stack-title">Javascript</h3>
+                        <div className="stack-box"><div className="rating rating--js hidden"></div></div>
+                        <h4 className="percent">80%</h4>
+                    </div>
+                    <div className="tech-stack__item">
+                        <h3 className="stack-title">React</h3>
+                        <div className="stack-box"><div className="rating rating--react hidden"></div></div>
+                        <h4 className="percent">70%</h4>
+                    </div>
+                    <div className="tech-stack__item">
+                        <h3 className="stack-title">Vue.js</h3>
+                        <div className="stack-box"><div className="rating rating--vue hidden"></div></div>
+                        <h4 className="percent">60%</h4>
+                    </div>
+                    <div className="tech-stack__item">
+                        <h3 className="stack-title">HTML</h3>
+                        <div className="stack-box"><div className="rating rating--html hidden"></div></div>
+                        <h4 className="percent">80%</h4>
+                    </div>
+                    <div className="tech-stack__item">
+                        <h3 className="stack-title">CSS</h3>
+                        <div className="stack-box"><div className="rating rating--css hidden"></div></div>
+                        <h4 className="percent">80%</h4>
+                    </div>
+                    <div className="tech-stack__item">
+                        <h3 className="stack-title">Ruby/Rails</h3>
+                        <div className="stack-box"><div className="rating rating--ruby hidden"></div></div>
+                        <h4 className="percent">60%</h4>
+                    </div>
+                    <div className="tech-stack__item">
+                        <h3 className="stack-title">Node.js</h3>
+                        <div className="stack-box"><div className="rating rating--node hidden"></div></div>
+                        <h4 className="percent">70%</h4>
+                    </div>
+                </Element>
             </div>
         );
     }
