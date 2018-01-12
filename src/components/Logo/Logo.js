61f1a310f416ee8781208a974765f9feb0110afa
Tyler <tcameronsmith@gmail.com>
2018-01-12 14:01:54 +1100

    remove link from logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Typist from 'react-typist';
 import './style.css';
 
@@ -8,11 +8,11 @@ const Logo = (props) => {
     return (
         <h1 className="nav-title">
             &#123;&nbsp;
-            <Link to="/">
+            
                 <Typist className="nav-span" cursor={{ show: true, hideWhenDone: true, blink: true, element: '|', hideWhenDoneDelay: 2000 }} avgTypingDelay={250} startDelay={1200} >
                     {title}
                 </Typist>
-            </Link>
+            
             &nbsp;&#125;
         </h1>
     )
