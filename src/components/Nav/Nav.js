0cb3aeff85664b5f4977a97530397889a64d241c
Tyler <tcameronsmith@gmail.com>
2018-01-25 21:48:23 +1100

    style nav to have hamburger menu on small screens

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,9 +2,35 @@ import React from 'react';
 import { Link } from 'react-scroll'
 import './style.css';
 
+// $('#menu-toggle').click(function(){
+// $(this).toggleClass('open');
+
+// }) 
+function menuToggle(e) {
+    document.querySelector('#menu-toggle').classList.toggle('open');
+    document.querySelector('.nav-list').classList.toggle('show');
+};
+
 const Nav = () => {
+    // const menuToggle = (e) => {
+    //     // console.log(e);
+    //     document.querySelector('#menu-toggle').classList.toggle('open');
+    //     // this.toggleClass('open');
+    //     console.log(document.querySelector('#menu-toggle').classList);
+    // }
     return (
         <nav>
+            <div id="menu-toggle" onClick={menuToggle}>
+                <div id="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div id="cross">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
             <ul className="nav-list">
                 <li className="nav-item">
                     <Link activeClass="active" to="projects" spy={true} smooth={true} offset={0} duration={500} onSetActive={this.handleSetActive}>Projects</Link>
