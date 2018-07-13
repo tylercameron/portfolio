4378d6b71eacc95630722ead8242c8b8eb6dbc08
Tyler <tcameronsmith@gmail.com>
2018-07-14 09:07:30 +1100

    clean up code, remove unused

--- src/helpers/rotateTitle.js
+++ src/helpers/rotateTitle.js
@@ -3,24 +3,6 @@ function rotateTitle() {
     const header = document.querySelector('.header');
     const titlesArray = [];
     titles.forEach(item => titlesArray.push(item));
-
-    // setInterval(function () {
-    //     let activeIndex = titlesArray.findIndex(x => (x.className.includes('visible')));
-
-    //     if (activeIndex === titlesArray.length - 1) {
-    //         titlesArray[activeIndex].classList.remove('visible');
-    //         titlesArray[activeIndex].classList.add('hidden');
-
-    //         titlesArray[0].classList.remove('hidden');
-    //         titlesArray[0].classList.add('visible');
-    //     } else {
-    //         titlesArray[activeIndex].classList.remove('visible');
-    //         titlesArray[activeIndex].classList.add('hidden');
-
-    //         titlesArray[activeIndex + 1].classList.remove('hidden');
-    //         titlesArray[activeIndex + 1].classList.add('visible');
-    //     }
-    // }, 1200);
     
     let timer = 8500;
 
