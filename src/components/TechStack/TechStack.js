820531d99f68310c573273a4a719f02657238de7
Tyler <tcameronsmith@gmail.com>
2018-08-31 15:48:33 +1000

    add skills to tech stack, position for desktop screen

--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -19,12 +19,12 @@ class TechStack extends Component {
             }
             const children = activeView.children;
     
-            for (let i = 0; i < children.length; i++) {
-                const ratingsDiv = children[i].children[1].children[0];
-                if (ratingsDiv.classList.contains('hidden')) {
-                    ratingsDiv.classList.remove('hidden');
-                }
-            }
+            // for (let i = 0; i < children.length; i++) {
+            //     const ratingsDiv = children[i].children[1].children[0];
+            //     if (ratingsDiv.classList.contains('hidden')) {
+            //         ratingsDiv.classList.remove('hidden');
+            //     }
+            // }
         }, 10);
 
     }
@@ -38,53 +38,47 @@ class TechStack extends Component {
 
         const children = activeView.children;
 
-        for (let i = 0; i < children.length; i++) {
-            const ratingsDiv = children[i].children[1].children[0];
+        // for (let i = 0; i < children.length; i++) {
+        //     const ratingsDiv = children[i].children[1].children[0];
             
-            if (!ratingsDiv.classList.contains('hidden')) {
-                ratingsDiv.classList.add('hidden');
-            }
-        }
+        //     if (!ratingsDiv.classList.contains('hidden')) {
+        //         ratingsDiv.classList.add('hidden');
+        //     }
+        // }
     }
 
     render() {
         return (
             <div id="tech-stack" className="tech-stack opac">
-                <div className="tech-stack__item">
-                    <h3 className="stack-title">Javascript</h3>
-                    <div className="stack-box"><div className="rating rating--js hidden"></div></div>
-                    <h4 className="percent">80%</h4>
-                </div>
-                <div className="tech-stack__item">
-                    <h3 className="stack-title">React</h3>
-                    <div className="stack-box"><div className="rating rating--react hidden"></div></div>
-                    <h4 className="percent">70%</h4>
-                </div>
-                <div className="tech-stack__item">
-                    <h3 className="stack-title">Vue.js</h3>
-                    <div className="stack-box"><div className="rating rating--vue hidden"></div></div>
-                    <h4 className="percent">60%</h4>
-                </div>
-                <div className="tech-stack__item">
-                    <h3 className="stack-title">HTML</h3>
-                    <div className="stack-box"><div className="rating rating--html hidden"></div></div>
-                    <h4 className="percent">80%</h4>
-                </div>
-                <div className="tech-stack__item">
-                    <h3 className="stack-title">CSS</h3>
-                    <div className="stack-box"><div className="rating rating--css hidden"></div></div>
-                    <h4 className="percent">80%</h4>
-                </div>
-                <div className="tech-stack__item">
-                    <h3 className="stack-title">Ruby/Rails</h3>
-                    <div className="stack-box"><div className="rating rating--ruby hidden"></div></div>
-                    <h4 className="percent">60%</h4>
-                </div>
-                <div className="tech-stack__item">
-                    <h3 className="stack-title">Node.js</h3>
-                    <div className="stack-box"><div className="rating rating--node hidden"></div></div>
-                    <h4 className="percent">70%</h4>
-                </div>
+                <h3 className="skill skill--1">Javascript</h3>
+                <h3 className="skill skill--2">HTML</h3>
+                <h3 className="skill skill--3">CSS</h3>
+                <h3 className="skill skill--4">Git</h3>
+
+                <h4 className="skill skill--5">React.js</h4>
+                <h4 className="skill skill--6">Node.js</h4>
+                <h4 className="skill skill--7">Express.js</h4>
+                <h4 className="skill skill--8">JQuery</h4>
+                <h4 className="skill skill--9">Github</h4>
+                <h4 className="skill skill--10">Bitbucket</h4>
+                <h4 className="skill skill--11">Craft CMS</h4>
+                <h4 className="skill skill--12">Vue.js</h4>
+                <h4 className="skill skill--13">MySQL</h4>
+                <h4 className="skill skill--14">Ruby on Rails</h4>
+                <h4 className="skill skill--15">Sass</h4>
+                <h4 className="skill skill--16">Statamic CMS</h4>
+                <h4 className="skill skill--17">Mongo DB</h4>
+                
+                <h5 className="skill skill--18">Gulp</h5>
+                <h5 className="skill skill--19">PostgreSQL</h5>
+                <h5 className="skill skill--20">Webpack</h5>
+                <h5 className="skill skill--21">Gatsby.js</h5>
+                <h5 className="skill skill--22">Sketch</h5>
+                <h5 className="skill skill--23">Next.js</h5>
+                <h5 className="skill skill--24">Grunt</h5>
+                <h5 className="skill skill--25">Redux</h5>
+                <h5 className="skill skill--26">Wordpress</h5>
+                <h5 className="skill skill--27">Photoshop</h5>                
             </div>
         );
     }
